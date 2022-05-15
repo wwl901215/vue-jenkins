dc55d71aa0b82698bad709674674ce7b394586dc
wenliang <wwl901215@163.com>
2022-05-15 10:32:48 +0800

    5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App(4)"/>
+  <HelloWorld msg="Welcome to Your Vue.js App(5)"/>
 </template>
 
 <script>
